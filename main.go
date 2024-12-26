26e0be6a4c5d955d5745e1b49c04ed651bef2a5b
pedropilla <pedropilla@gmail.com>
2024-12-26 13:30:30 +0000

    add logging and tls

--- main.go
+++ main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+// Logging middleware
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		log.Printf("Started %s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+		log.Printf("Completed %s %s in %v", r.Method, r.URL.Path, time.Since(start))
+	})
+}
 
 func main() {
 	r := mux.NewRouter()
@@ -16,6 +27,9 @@ func main() {
 	r.HandleFunc("/users", CreateUser).Methods("POST")
 	r.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
 	r.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
+
+	// Add middleware
+	r.Use(LoggingMiddleware)
 
 	log.Println("Server running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
