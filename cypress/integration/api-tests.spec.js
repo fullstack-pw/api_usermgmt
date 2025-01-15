bdd943f52fb3496fd3d2339f9df6544b6bfd1312
Pedro Pilla <pedropilla@gmail.com>
2025-01-15 16:53:17 +0000

    cypress dev

--- cypress/integration/api-tests.spec.js
+++ cypress/integration/api-tests.spec.js
@@ -1,5 +1,5 @@
 describe('User Management API Tests', () => {
-    const baseUrl = 'https://api-usermgmt.fullstack.pw';
+    const baseUrl = 'https://dev.api-usermgmt.fullstack.pw';
   
     it('Health Check', () => {
       cy.request(`${baseUrl}/health`)
